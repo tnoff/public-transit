66bf194862c27a93141b2f6532a9dbfaee1597ea
tylernorth <tylernorth18@gmail.com>
2015-02-15 20:00:32 -0800

    extend stop prediction tests

---
+++ tests/data/stop_predictions.py
@@ -0,0 +1,18 @@
+text = '''
+<?xml version="1.0" encoding="utf-8" ?> 
+<body copyright="All data copyright AC Transit 2015.">
+<predictions agencyTitle="AC Transit" routeTitle="22" routeTag="22" stopTitle="Mission Blvd &amp; Central Blvd" stopTag="9902820">
+  <direction title="Counterclockwise to Hayward BART">
+  <prediction epochTime="1424062084430" seconds="3196" minutes="53" isDeparture="false" affectedByLayover="true" dirTag="22_25_1" vehicle="1216" block="22001" tripTag="3965673" />
+  </direction>
+  <message text="Hey hey hey this is a message"/>
+</predictions>
+<predictions agencyTitle="AC Transit" routeTitle="99" routeTag="99" stopTitle="Mission Blvd &amp; Central Blvd" stopTag="9902820">
+  <direction title="To Bay Fair BART">
+  <prediction epochTime="1424062181349" seconds="3293" minutes="54" isDeparture="false" affectedByLayover="true" dirTag="99_72_1" vehicle="1411" block="99005" tripTag="3966110" />
+  </direction>
+</predictions>
+<predictions agencyTitle="AC Transit" routeTitle="801" routeTag="801" stopTitle="Mission Blvd &amp; Central Blvd" stopTag="9902820" dirTitleBecauseNoPredictions="To Downtown Oakland">
+</predictions>
+</body>
+'''
