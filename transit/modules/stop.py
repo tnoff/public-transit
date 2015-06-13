3af547fc4afd8aa4b67207946d8b49bd4faf14dc
tylernorth <tylernorth18@gmail.com>
2015-06-12 21:26:19 -0700

    wip fix stupid prediction bug

--- transit/modules/stop.py
+++ transit/modules/stop.py
@@ -84,6 +84,7 @@ def stop_prediction(agency_tag, stop_id, route_tag=None):
             new_dir = RouteDirectionPrediction(direction)
             for pred in direction.find_all('prediction'):
                 new_dir.predictions.append(RouteStopPrediction(pred))
+            route_pred.directions.append(new_dir)
         for message in new_route.find_all('message'):
             route_pred.messages.append(message.get('text').encode('utf-8'))
         route_predictions.append(route_pred)
