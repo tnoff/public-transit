fd59663e78365c39c2d9d4a91f2e4729d05817b8
tylernorth <tylernorth18@gmail.com>
2015-06-15 14:17:36 -0700

    add some better testin

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -5,6 +5,8 @@ from transit import client
 from transit.exceptions import TransitException
 from transit.common import urls, utils
 
+from transit.modules import agency, route
+
 from tests.data import agency_list as agency_list
 from tests.data import error as error
 from tests.data import message_get as message_get
@@ -38,90 +40,142 @@ class TestClient(unittest.TestCase):
                                test_url,
                                body=agency_list.text,
                                content_type='application/xml')
-        client.agency_list()
+        agencies = client.agency_list()
+        # Make sure list generated correctly
+        for a in agencies:
+            self.assertTrue(isinstance(a, agency.Agency))
+            self.assertNotEqual(None, a.tag)
 
     @httpretty.activate
     def test_route_list(self):
-        test_url = urls.route['list'] % 'sf-muni'
+        agency_tag = 'sf-muni'
+        test_url = urls.route['list'] % agency_tag
         httpretty.register_uri(httpretty.GET,
                                test_url,
                                body=route_list.text,
                                content_type='application/xml')
-        client.route_list('sf-muni')
+        routes = client.route_list(agency_tag)
+        for r in routes:
+            self.assertTrue(isinstance(r, route.Route))
+            self.assertNotEqual(r.route_tag, None)
+            self.assertEqual(r.agency_tag, agency_tag)
 
     @httpretty.activate
     def test_route_show(self):
-        test_url = urls.route['show'] % ('actransit', '22')
+        agency_tag = 'actransit'
+        route_tag = '22'
+        test_url = urls.route['show'] % (agency_tag, route_tag)
         httpretty.register_uri(httpretty.GET,
                                test_url,
                                body=route_show.text,
                                content_type='application/xml')
-        client.route_get('sf-muni', 'N')
+        r = client.route_get(agency_tag, route_tag)
+        self.assertEqual(r.agency_tag, agency_tag)
+        self.assertEqual(r.route_tag, route_tag)
+        self.assertNotEqual(len(r.stops), 0)
+        self.assertNotEqual(len(r.directions), 0)
+        self.assertNotEqual(len(r.directions[0].stop_tags), 0)
+        self.assertNotEqual(len(r.paths), 0)
 
     @httpretty.activate
     def test_stop_prediction_no_route(self):
-        test_url = urls.predictions['stop'] % ('actransit', '51303')
+        agency_tag = 'actransit'
+        stop_id = '51303'
+        test_url = urls.predictions['stop'] % (agency_tag, stop_id)
         httpretty.register_uri(httpretty.GET,
                                test_url,
                                body=stop_predictions.text,
                                content_type='application/xml')
-        client.stop_prediction('actransit', 51303)
+        preds = client.stop_prediction(agency_tag, stop_id)
+        first_pred = preds[0]
+        self.assertNotEqual(len(first_pred.directions), 0)
+        self.assertNotEqual(len(first_pred.directions[0].predictions), 0)
+        self.assertNotEqual(len(first_pred.messages), 0)
 
     @httpretty.activate
     def test_stop_prediction_with_route(self):
-        test_url = urls.predictions['route'] % ('actransit', '51303', '22')
+        agency_tag = 'actransit'
+        stop_id = '51303'
+        route_tag = '22'
+        test_url = urls.predictions['route'] % \
+            (agency_tag, stop_id, route_tag)
         httpretty.register_uri(httpretty.GET,
                                test_url,
                                body=stop_predictions_route.text,
                                content_type='application/xml')
-        client.stop_prediction('actransit', 51303)
+        preds = client.stop_prediction(agency_tag, stop_id, route_tag=route_tag)
+        first_pred = preds[0]
+        self.assertNotEqual(len(first_pred.directions), 0)
+        self.assertNotEqual(len(first_pred.directions[0].predictions), 0)
 
     @httpretty.activate
     def test_schedule(self):
-        test_url = urls.schedule['show'] % ('actransit', '22')
+        agency_tag = 'actransit'
+        route_tag = '22'
+        test_url = urls.schedule['show'] % (agency_tag, route_tag)
         httpretty.register_uri(httpretty.GET,
                                test_url,
                                body=schedule_get.text,
                                content_type='application/xml')
-        client.schedule_get('actransit', '22')
+        schedules = client.schedule_get(agency_tag, route_tag)
+        first_sched = schedules[0]
+        self.assertNotEqual(len(first_sched.blocks), 0)
+        first_block = first_sched.blocks[0]
+        self.assertNotEqual(len(first_block.stop_schedules), 0)
 
     @httpretty.activate
     def test_vehicle_locations(self):
-        test_url = urls.vehicle['location'] % ('sf-muni', 'N', '1144953500233')
+        agency_tag = 'sf-muni'
+        route_tag = 'N'
+        epoch_time = '1144953500233'
+        test_url = urls.vehicle['location'] % \
+            (agency_tag, route_tag, epoch_time)
         httpretty.register_uri(httpretty.GET,
                                test_url,
                                body=vehicle_locations.text,
                                content_type='application/xml')
-        client.vehicle_location('sf-muni', 'N', '1144953500233')
+        locations = client.vehicle_location(agency_tag, route_tag, epoch_time)
+        self.assertNotEqual(len(locations), 0)
 
     @httpretty.activate
-    def test_message(self):
+    def test_message_single(self):
         # Test with one arg
-        test_url = urls.message['message']['url'] % ('sf-muni')
-        tags = ['38']
-        for i in tags:
+        agency_tag = 'sf-muni'
+        route_tags = ['38']
+        test_url = urls.message['message']['url'] % (agency_tag)
+        for i in route_tags:
             test_url += urls.message['message']['suffix'] % i
         httpretty.register_uri(httpretty.GET,
                                test_url,
                                body=message_get.text,
                                content_type='application/xml')
-        client.message_get('sf-muni', '38')
+        routes = client.message_get(agency_tag, route_tags)
+        first_route = routes[0]
+        self.assertNotEqual(len(first_route.messages), 0)
+
+    @httpretty.activate
+    def test_message_multiple(self):
         # Test with mulitple args
-        test_url = urls.message['message']['url'] % ('sf-muni')
-        tags = ['38', '47']
-        for i in tags:
+        agency_tag = 'sf-muni'
+        route_tags = ['38', '47']
+        test_url = urls.message['message']['url'] % (agency_tag)
+        for i in route_tags:
             test_url += urls.message['message']['suffix'] % i
         httpretty.register_uri(httpretty.GET,
                                test_url,
                                body=message_get_multi.text,
                                content_type='application/xml')
-        client.message_get('sf-muni', ['38', '47'])
+        routes = client.message_get(agency_tag, route_tags)
+        self.assertTrue(len(routes) > 1)
+        first_route = routes[0]
+        self.assertNotEqual(len(first_route.messages), 0)
 
     @httpretty.activate
-    def test_multi_prediction(self):
+    def test_multi_prediction_single(self):
         # Test with one stop/route
-        test_url = urls.predictions['multi']['url'] % ('sf-muni')
+        agency_tag = 'sf-muni'
         data = {'38' : ['13568']}
+        test_url = urls.predictions['multi']['url'] % (agency_tag)
         for key in data:
             for i in data[key]:
                 test_url += urls.predictions['multi']['suffix'] % (key, i)
@@ -129,10 +183,18 @@ class TestClient(unittest.TestCase):
                                test_url,
                                body=multi_one.text,
                                content_type='application/xml')
-        client.multiple_stop_predictions('sf-muni', data)
+        preds = client.multiple_stop_predictions(agency_tag, data)
+        first_pred = preds[0]
+        self.assertNotEqual(len(first_pred.directions), 0)
+        self.assertNotEqual(len(first_pred.directions[0].predictions), 0)
+        self.assertNotEqual(len(first_pred.messages), 0)
+
+    @httpretty.activate
+    def test_multi_prediction_multiple(self):
         # Test with multiple stops on same route
-        test_url = urls.predictions['multi']['url'] % ('sf-muni')
+        agency_tag = 'sf-muni'
         data = {'38' : ['13568', '13567']}
+        test_url = urls.predictions['multi']['url'] % ('sf-muni')
         for key in data:
             for i in data[key]:
                 test_url += urls.predictions['multi']['suffix'] % (key, i)
@@ -140,4 +202,8 @@ class TestClient(unittest.TestCase):
                                test_url,
                                body=multi_two.text,
                                content_type='application/xml')
-        client.multiple_stop_predictions('sf-muni', data)
+        preds = client.multiple_stop_predictions(agency_tag, data)
+        first_pred = preds[0]
+        self.assertNotEqual(len(first_pred.directions), 0)
+        self.assertNotEqual(len(first_pred.directions[0].predictions), 0)
+        self.assertNotEqual(len(first_pred.messages), 0)
