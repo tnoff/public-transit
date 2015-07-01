08d894be008002317f6e4adb4e00fa981e90bcc5
tylernorth <tylernorth18@gmail.com>
2015-07-01 12:04:31 -0700

    move advisories to seperate folder

--- transit/modules/bart/client.py
+++ transit/modules/bart/client.py
@@ -1,53 +1,10 @@
-from datetime import datetime
-
-from transit.common.urls import bart
-from transit.common import utils
-
-class ServiceAdvisory(object):
-    def __init__(self, bsa_data):
-        try:
-            self.id = bsa_data.get('id').encode('iso-8859-1')
-        except AttributeError:
-            self.id = None
-        try:
-            self.station = bsa_data.find('station').contents[0].encode('iso-8859-1')
-        except (IndexError, AttributeError):
-            self.station = None
-        try:
-            self.type = bsa_data.find('type').contents[0].encode('iso-8859-1')
-        except (IndexError, AttributeError):
-            self.type = None
-        descriptions = []
-        for i in bsa_data.find('description').contents:
-            s = i.encode('iso-8859-1')
-            if s != '\n':
-                descriptions.append(s)
-        self.descriptions = descriptions
-        try:
-            posted = bsa_data.find('posted').contents[0].encode('iso-8859-1')
-            self.posted = datetime.strptime(posted, '%a %b %d %Y %I:%M %p %Z')
-        except (IndexError, AttributeError):
-            self.posted = None
-        try:
-            expires = bsa_data.find('expires').contents[0].encode('iso-8859-1')
-            self.expires = datetime.strptime(expires, '%a %b %d %Y %I:%M %p %Z')
-        except (IndexError, AttributeError):
-            self.expires = None
-
-    def __repr__(self):
-        return '%s - %s' % (self.station, self.descriptions)
+from transit.modules.bart.advisories import client as advisory_client
 
 def service_advisory():
-    url = bart.service_advisory()
-    soup = utils.make_request(url)
-    return [ServiceAdvisory(bsa) for bsa in soup.find_all('bsa')]
+    return advisory_client.service_advisory()
 
 def train_count():
-    url = bart.train_count()
-    soup = utils.make_request(url)
-    return int(soup.find('traincount').contents[0].encode('iso-8859-1'))
+    return advisory_client.train_count()
 
 def elevator_status():
-    url = bart.elevator_status()
-    soup = utils.make_request(url)
-    return ServiceAdvisory(soup.find('bsa'))
+    return advisory_client.elevator_status()
